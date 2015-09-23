2964e2efce06f0f443362665b6dc7b877a9c887d
cnorgate <cnorgate@tcc-cnorgate2.local>
2015-09-22 19:35:02 -0700

    Got the first scroll view working

---
+++ CarouselHomework/IntroViewController.swift
@@ -0,0 +1,41 @@
+//
+//  IntroViewController.swift
+//  CarouselHomework
+//
+//  Created by Cameron Norgate on 9/22/15.
+//  Copyright © 2015 Cameron Norgate. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+//        scrollView.contentSize =  CGSize(width: 320, height: 1000)
+        scrollView.contentSize = imageView.image!.size
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
